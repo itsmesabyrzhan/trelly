61b849c427a5716da49c1bd8623ed914a710710f
Sabyrzhan <itsme.sabyrzhan@gmail.com>
2025-10-24 22:01:41 +0500

    feat: highlight high priority tasks

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ const tasks = [
     title: 'Купить продукты на неделю',
     isDone: false,
     addedAt: '1 сентября',
+    priority: 2,
   },
   {
     id: 2,
     title: 'Полить цветы',
     isDone: true,
     addedAt: '2 сентября',
+    priority: 0,
   },
   {
     id: 3,
     title: 'Сходить на тренировку',
     isDone: false,
     addedAt: '3 сентября',
+    priority: 1,
   },
 ]
 
@@ -36,7 +39,16 @@ export function App() {
       <ul>
         { tasks.map((task) => {
           return (
-            <li key={ task.id }>
+            <li
+              key={ task.id }
+              style={
+                task.priority >= 2
+                  ? {
+                    color: '#000',
+                    backgroundColor: 'rgba(255, 255, 255, 0.87)',
+                  }
+                  : {} }
+            >
               <p>
                 <b>Заголовок: </b>
                 <span
